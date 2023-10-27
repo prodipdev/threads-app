04de7821efbc7447cda5dd89f55c03d24d5a5d37
prodipdev <prodipkarati.dev@gmail.com>
2023-10-27 22:52:41 +0600

    Create LeftSidebar section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={`${inter.className} bg-dark-1`}>
           <Topbar />
           <main>
             <LeftSidebar />
