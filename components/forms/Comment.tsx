be2c038b2655792039b9cfdfcf9c3642b421792b
prodipdev <prodipkarati.dev@gmail.com>
2023-11-07 15:56:53 +0600

    Create comment structer

---
+++ components/forms/Comment.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  return <div className="">Comment</div>;
+};
+
+export default Comment;
